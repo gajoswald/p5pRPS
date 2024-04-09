a1b4e0b1a8912c4751b600401c50c6285c7e7f6e
Mr. Oswald <62300716+gajoswald@users.noreply.github.com>
2024-04-09 17:10:57 +0000

    Got static part working... seems like I should transition to a class?

--- Play.js
+++ Play.js
@@ -1,6 +1,60 @@
-class RPSArt {
-  static string = {
-    Rock: `
+class Play {
+  static all = {}
+  static art = {}
+  
+  static Type = {
+    Rock: "Rock",
+    Paper: "Paper",
+    Scissors: "Scissors"
+  }
+
+  static Beats = {
+    Rock: Play.Type.Scissors,
+    Paper: Play.Type.Rock,
+    Scissors: Play.Type.Paper
+  }
+
+  static initializeStatic() {
+    for( const type in Play.Type ) {
+      Play.all[type] = new Group()
+      Play.art[type] = spriteArt(Play.Art[type],2,palette)
+    }
+  }
+
+  static createPlay(type, {
+    x = random(width),
+    y = random(height),
+    velocity = {x: random(-3,3), y: random(-3,3)},
+  } = {}) {
+    const newSprite = new Play.all[type].Sprite()
+    newSprite.img = Play.art[type]
+    newSprite.x = x
+    newSprite.y = y
+    newSprite.velocity.x = velocity.x
+    newSprite.velocity.y = velocity.y
+    newSprite.rotation = atan2(newSprite.velocity.y, newSprite.velocity.x)
+    newSprite.bounciness = 1.1
+  }
+
+  static populate(n) {
+    for( const type in Play.Type ) {
+      for( let i = 0; i < n; i++ ) {
+        Play.createPlay(type)
+      }
+    }
+
+    for( const k in Play.Beats ) {
+      console.log( k,  )
+      Play.all[k].overlaps(Play.all[Play.Beats[k]], (a,b) => {
+        const c = Play.createPlay( k, {x:b.x, y:b.y, velocity:b.velocity})
+        b.remove()
+      })
+    }
+  }
+}
+
+Play.Art = {
+  Rock: `
 aaaaaa
 .a   aa
 .a    aa
@@ -14,7 +68,7 @@ aaaaaa
 .a     a
 aa     aa
 `,
-  Paper: `
+Paper: `
 bbbbbb
 .b   bb
 .b    bb
@@ -28,7 +82,7 @@ bbbbbb
 .b
 bb
 `,
-  Scissors: `
+Scissors: `
 .ccccc
 cc   cc
 cc    cc
@@ -42,19 +96,4 @@ cc.....c
 .cc...cc
 ..ccccc
 `
-  }
-}
-class Play {  
-  static Rock = new Play("Rock")
-  static Paper = new Play("Paper")
-  static Scissors = new Play("Scissors")
-  
-  static random() {
-    return random([Play.Rock, Play.Paper,Play.Scissors])
-  }
-
-  constructor( name ) { 
-    this.name = name 
-    this.art = spriteArt(RPSArt.string[name],2,palette)
-  }
 }
--- script.js
+++ script.js
@@ -1,45 +1,15 @@
-new p5()
-
 // https://sashamaps.net/docs/resources/20-colors/
 let palette = {a:'#e6194b', b:'#3cb44b', c:'#ffe119', d:'#4363d8', e:'#f58231', f:'#911eb4', g:'#46f0f0', h:'#f032e6', i:'#bcf60c', j:'#fabebe', k:'#008080', l:'#e6beff', m:'#9a6324', n:'#fffac8', o:'#800000', p:'#aaffc3', q:'#808000', r:'#ffd8b1', s:'#000075', t:'#808080', y:'#ffffff', z:'#000000'}
 
-let groups = {
-  Rock:{},
-  Paper:{},
-  Scissors:{}
-}
-
 let data = []
-
+const N = 33
 function setup() {
   new Canvas(windowWidth-10, windowHeight-10)
   for( [k,v] of Object.entries(palette) ) { palette[k] = color(v) }
   background(255)
-
-  populate()
+  Play.initializeStatic()
+  Play.populate(N)
   boundaries() 
-}
-
-function populate() {
-  for( const g in groups ) {
-    groups[g] = new Group()    
-    for( let i = 0; i < 33; i++ ) {
-      createPlay(g)
-    }
-  }
-  
-  groups.Rock.overlaps(groups.Scissors, (a,b) => {
-    const c = createPlay( Play.Rock.name, {x:b.x, y:b.y, velocity:b.velocity} )
-    b.remove()    
-  })
-  groups.Paper.overlaps(groups.Rock, (a,b) => {
-    const c = createPlay( Play.Paper.name, {x:b.x, y:b.y, velocity:b.velocity} )
-    b.remove()    
-  })
-  groups.Scissors.overlaps(groups.Paper, (a,b) => {
-    const c = createPlay( Play.Scissors.name, {x:b.x, y:b.y, velocity:b.velocity} )
-    b.remove()    
-  })
 }
 
 function boundaries() {
@@ -54,32 +24,16 @@ function createBoundary( x,y,w,h ) {
   b.friction = 0
 }
 
-function createPlay(type, {
-  x = random(width),
-  y = random(height),
-  velocity = {x: random(-3,3), y: random(-3,3)},
-} = {}) {
-  const newSprite = new groups[type].Sprite()
-  newSprite.img = Play[type].art
-  newSprite.x = x
-  newSprite.y = y
-  newSprite.velocity.x = velocity.x
-  newSprite.velocity.y = velocity.y
-  newSprite.rotation = atan2(newSprite.velocity.y, newSprite.velocity.x)
-  newSprite.bounciness = 1.1
-}
-
-
 function draw() {
   clear()
   fill( palette.a )
-  text( `Rock: ${groups.Rock.length}`, 10, 10 )
+  text( `Rock: ${Play.all.Rock.length}`, 10, 10 )
   fill( palette.b )
-  text(`Paper: ${groups.Paper.length}`, 10, 22 )
+  text(`Paper: ${Play.all.Paper.length}`, 10, 22 )
   fill( palette.c )
-  text(`Scissors: ${groups.Scissors.length}`, 10, 34 )
-  data.push( [groups.Rock.length,groups.Paper.length,groups.Scissors.length] )
-  if( groups.Rock.length === 99 || groups.Paper.length === 99 || groups.Scissors.length === 99 ) {
+  text(`Scissors: ${Play.all.Scissors.length}`, 10, 34 )
+  data.push( [Play.all.Rock.length,Play.all.Paper.length,Play.all.Scissors.length] )
+  if( Play.all.Rock.length === N*3 || Play.all.Paper.length === N*3 || Play.all.Scissors.length === N*3 ) {
     noLoop()
     allSprites.remove()
     fill('white')
@@ -102,7 +56,7 @@ function draw() {
 function keyPressed() {
   if( key === 'r' ) {
     data = []
-    populate()
+    Play.populate(N)
     boundaries()
     loop()
   }
